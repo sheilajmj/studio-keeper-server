44a38601e2d0467b820cd28122899ac4b2c37065
Sheila Jagla <Starbone@gmail.com>
2020-02-26 22:04:16 -0700

    setting up

--- src/app.js
+++ src/app.js
@@ -4,16 +4,21 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const {CLIENT_ORIGIN} = require('./config');
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(cors())
+app.use(  cors({
+        origin: CLIENT_ORIGIN
+    })
+    )
 app.use(morgan(morganOption))
 app.use(helmet())
+
+
 
 app.get('/', (req, res) => {
    res.send('Hello, world!')
