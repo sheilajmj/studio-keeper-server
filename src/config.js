729265a9abf4649de467ca2fe567220fa7e2647d
starbone Jagla <starbone@gmail.com>
2020-03-30 16:35:31 -0600

    modifications to use deployed db

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgres://studiokeeper:admin@localhost/studiokeeper',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://escbmkagbvjqme:e449edbb7cbf5e741cc3a92ad5d454dcd91e799ac538701ee88a3831bb3f9f9c@ec2-3-229-210-93.compute-1.amazonaws.com:5432/d2t064rk398q1q',
     JWT_SECRET: process.env.JWT_SECRET || 'here-is-a-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
-  
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://studiokeeper:admin@localhost/studiokeeper-test'
   }
